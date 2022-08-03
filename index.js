0b8097160f172da93d748da2658e4daeb395a905
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:30:02 +0700

    TUGAS 3 JAVASCRIPT
    SAYA KUMPULKAN TUGAS KEPADA NIOMIC

---
+++ index.js
@@ -0,0 +1,15 @@
+function data() {
+    return "Arizona";
+    var namalengkap = "Riki Arizona";
+    return namalengkap;
+}
+console.log(data())
+var datadiri = data();
+console.log(datadiri);
+
+function datalengkap(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+datalengkap("Riki","Arizona",31)
